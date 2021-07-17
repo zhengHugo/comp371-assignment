96da04587596a5a067cf94ccb2424dab6d592a4e
Hugo Zheng <zhenghugo515@gmail.com>
2021-07-16 23:25:34 -0400

    Fix rotation

--- src/Model.cpp
+++ src/Model.cpp
@@ -51,9 +51,10 @@ glm::mat4 Model::getModelMatrix(int index) {
 void Model::updateModelMatrices() {
   glm::mat4 model = glm::mat4(1.0f);
   model = glm::scale(model, glm::vec3(Scale, Scale, Scale));
-  model = model * glm::mat4_cast(quaternion);
   for (int i = 0; i < relativePositions.size(); i++) {
-    modelMatrices[i] = glm::translate(model, glm::vec3(basePosition + relativePositions[i]));
+    modelMatrices[i] = glm::translate(model, glm::vec3(basePosition));
+    modelMatrices[i] = modelMatrices[i] * glm::mat4_cast(quaternion);
+    modelMatrices[i] = glm::translate(modelMatrices[i], glm::vec3(relativePositions[i]));
   }
 }
 
