0f27e8a06dc4a702a96e23c93d80a71808f9c292
LittleRedRidingU <LittleRedRidingU@users.noreply.github.com>
2021-07-19 17:49:54 -0400

    adjust variable

--- src/main.cpp
+++ src/main.cpp
@@ -51,6 +51,7 @@ Model *wall3;
 Model *wall4;
 Model *currentModel;
 Model *currentWall;
+std::vector<Model*> otherObj;
 
 // for mouse initialization
 bool firstMouse = true; // true when mouse callback is called for the first time; false otherwise
@@ -354,6 +355,12 @@ int main(int argc, char *argv[]) {
       glm::vec3(4.0f, 3.0f, 0.0f)
   };
 
+  std::vector<glm::vec3> othersPosition = {
+      glm::vec3(-30.0, 0.0f, 30.0f),
+      glm::vec3(30.0f,  0.0f, 30.0f),
+      glm::vec3(30.0f,  0.0f, -30.0f),
+  };
+
   glm::vec3 baseCubePosition(2.0f, 3.0f, 5.0f);
   glm::vec3 baseWallPosition(2.0f, 3.0f, 2.0f);
 
@@ -365,10 +372,11 @@ int main(int argc, char *argv[]) {
   wall2 = new Model(baseWallPosition, relativeWallPositions2);
   wall3 = new Model(baseWallPosition, relativeWallPositions3);
   wall4 = new Model(baseWallPosition, relativeWallPositions4);
-
-
+  
   currentModel = model1;
   currentWall = wall1;
+  //set an array to store other object
+  otherObj = {model2, wall2, model3, wall3, model4, wall4};
 
   // bind geometry data
   unsigned int cubeVao, gridVao, axisVao, cubeVbo, gridVbo, axisVbo;
@@ -502,6 +510,29 @@ int main(int argc, char *argv[]) {
       glDrawArrays(GL_TRIANGLES, 0, 36);
     }
 
+    //otherModels
+    for (int j = 0; j < 3; j++) {
+      // translate to new position
+      glm::mat4 transModel = glm::translate(glm::mat4(1.0f), othersPosition[j]);
+      for (size_t i = 0; i < otherObj[j]->size(); i++) {
+        // assign cube texture
+        glUniform1i(cubeTexLocation, 0);
+        // calculate cube model matrix
+        glm::mat4 cubeModelMatrix = otherObj[2*j]->getModelMatrix(i) * transModel;
+        glUniformMatrix4fv(cubeModelLocation, 1, GL_FALSE, glm::value_ptr(cubeModelMatrix));
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+      }
+      for (size_t i = 0; i < otherObj[j+1]->size(); i++) {
+        // assign wall texture
+        glUniform1i(cubeTexLocation, 1);
+        // calculate the model matrix for wall
+        glm::mat4 wallModelMatrix = otherObj[2*j+1]->getModelMatrix(i) * transModel;
+        glUniformMatrix4fv(cubeModelLocation, 1, GL_FALSE, glm::value_ptr(wallModelMatrix));
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+      }
+    }
+
+
     // draw grid
     glBindVertexArray(gridVao);
     lineShader.use();
@@ -564,7 +595,6 @@ int main(int argc, char *argv[]) {
   glfwTerminate();
   return 0;
 }
-
 static void processInput(GLFWwindow *window) {
   // Escape
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
@@ -575,15 +605,19 @@ static void processInput(GLFWwindow *window) {
   if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) {
     currentModel = model1;
     currentWall = wall1;
+    otherObj = {model2, wall2, model3, wall3, model4, wall4};
   } else if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) {
     currentModel = model2;
     currentWall = wall2;
+    otherObj = {model1, wall1, model3, wall3, model4, wall4};
   } else if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS) {
     currentModel = model3;
     currentWall = wall3;
+    otherObj = {model1, wall1, model2, wall2, model4, wall4};
   } else if (glfwGetKey(window, GLFW_KEY_4) == GLFW_PRESS) {
     currentModel = model4;
     currentWall = wall4;
+    otherObj = {model1, wall1, model2, wall2, model3, wall3};
   }
 
   // u: scale up model
